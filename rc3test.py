0fb508eb15ab945b113d827ba277bd7a2e5eebd1
Sam Crognale <sam.crognale@gmail.com>
2015-05-23 01:53:28 -0700

    prio qdiscs implemented, not tested

--- rc3test.py
+++ rc3test.py
@@ -23,25 +23,34 @@ class RC3Topo(Topo):
         Topo.__init__(self)
 
         # Add hosts and switch
-        leftHost = self.addHost('h1')
-        rightHost = self.addHost('h2')
+        h1 = self.addHost('h1')
+        h2 = self.addHost('h2')
         switch = self.addSwitch('s1')
-        
+
         # Add links
-        self.addLink(leftHost, switch, bw=bandwidth, delay=DELAY, use_htb=True)
-        self.addLink(switch, rightHost, bw=bandwidth, delay=DELAY, use_htb=True)
+        self.addLink(h1, switch, bw=bandwidth, delay=DELAY, use_htb=True)
+        self.addLink(switch, h2, bw=bandwidth, delay=DELAY, use_htb=True)
+
 
 def rc3Test(bandwidth, flowLen):
     topo = RC3Topo(bandwidth)
     net = Mininet(topo, link=TCLink)
     net.start()
 
+
     print "Dumping node connections"
     dumpNodeConnections(net.hosts)
 
-    print "Testing bandwidth between 'h1' and 'h2'"
     h1, h2 = net.getNodeByName('h1', 'h2')
-    #net.iperf((h1, h2), l4Type='TCP')
+
+    #add prio qdisc to hosts
+    print "Adding qdisc"
+    h1.cmdPrint('tc qdisc add dev h1-eth0 parent 10:1 handle 15:0 prio bands 8')
+    h1.cmdPrint('tc qdisc show')
+    h2.cmdPrint('tc qdisc add dev h2-eth0 parent 10:1 handle 15:0 prio bands 8')
+    h2.cmdPrint('tc qdisc show')
+
+    print "Testing bandwidth between 'h1' and 'h2'"
     h2.sendCmd('iperf -s')
     result = h1.cmd('iperf -c', h2.IP(), '-n', flowLen)
     print result
@@ -49,4 +58,4 @@ def rc3Test(bandwidth, flowLen):
 
 if __name__ == '__main__':
     lg.setLogLevel('info')
-    rc3Test(LINK_BW_1, 2000)
+    rc3Test(LINK_BW_1, 20000000)
