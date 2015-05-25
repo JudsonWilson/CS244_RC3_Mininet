3bbd101fa9eadfc2821099117ce6efb6031118cf
Sam Crognale <sam.crognale@gmail.com>
2015-05-24 20:10:16 -0700

    started plotting test

--- rc3test.py
+++ rc3test.py
@@ -9,6 +9,9 @@ from mininet.util import pmonitor
 from time import time
 from signal import SIGINT
 
+import matplotlib.pyplot as plt
+
+
 # Actual experiment was 10Gbps and 1Gbps with an RTT of 20ms.
 # We reduce the link speed and increase the latency to maintain
 # the same Bandwidth-Delay Product
@@ -17,7 +20,7 @@ LINK_BW_1 = 100 # 100Mbps
 LINK_BW_2 = 10 # 10Mbps
 
 #DELAY = '500ms' # 0.5s, RTT=2s
-DELAY = '200ms' # TODO remove (test)
+DELAY = '20ms' # TODO remove (test)
 
 class RC3Topo(Topo):	
 
@@ -38,8 +41,16 @@ class RC3Topo(Topo):
 
 def addPrioQdisc(node, devStr):
     node.cmdPrint('tc qdisc add dev', devStr,
-            'parent 10:1 handle 15:0 prio bands 8 priomap 0 0 1 0 2 0 3 0 4 0 0 0 0 0 0 0')
+            'parent 10:1 handle 15:0 prio bands 8 priomap 0 1 2 3 4 5 6 7 7 7 7 7 7 7 7 7')
+    node.cmdPrint('tc filter add dev', devStr, 'parent 15:0 protocol ip prio 10 u32 match ip tos 0x00 0xff flowid 15:1')
+    node.cmdPrint('tc filter add dev', devStr, 'parent 15:0 protocol ip prio 10 u32 match ip tos 0x04 0xff flowid 15:2')
+    node.cmdPrint('tc filter add dev', devStr, 'parent 15:0 protocol ip prio 10 u32 match ip tos 0x08 0xff flowid 15:3')
+    node.cmdPrint('tc filter add dev', devStr, 'parent 15:0 protocol ip prio 10 u32 match ip tos 0x0c 0xff flowid 15:4')
+    node.cmdPrint('tc filter add dev', devStr, 'parent 15:0 protocol ip prio 10 u32 match ip tos 0x10 0xff flowid 15:5')
+
     node.cmdPrint('tc qdisc show')
+    node.cmdPrint('tc class show dev', devStr)
+    node.cmdPrint('tc filter show dev', devStr)
 
 def rc3Test(bandwidth, flowLen):
     topo = RC3Topo(bandwidth)
@@ -61,32 +72,35 @@ def rc3Test(bandwidth, flowLen):
     h2.sendCmd('iperf -s')
     #result = h1.cmd('iperf -c', h2.IP(), '-n', flowLen)
     #print result
-    '''
-    print 'launching high priority iperf'
-    h1.sendCmd('iperf -c', h2.IP(), '-n', flowLen, '-S 0x0 > highperf.txt')
-    print 'launching low priority iperf'
-    h1.sendCmd('iperf -c', h2.IP(), '-n', flowLen, '-S 0x4 > lowperf.txt')
-    print 'lol'
-    '''
 
     popens = {}
     print 'launching high priority iperf'
     popens['hiperf'] = h1.popen('iperf -c %s -n %i -S 0x0 > hiperf.txt' % (h2.IP(), flowLen))
     print 'launching low priority iperf'
-    popens['loperf'] = h1.popen('iperf -c %s -n %i -S 0x1 > loperf.txt' % (h2.IP(), flowLen))
+    popens['loperf'] = h1.popen('iperf -c %s -n %i -S 0x4 > loperf.txt' % (h2.IP(), flowLen))
 
+    '''
     endTime = time() + 40 #seconds
-    for perf, line in pmonitor(popens, timeoutms=100):
-    #    h1.cmdPrint('tc -s qdisc ls dev h1-eth0')
+    for perf, line in pmonitor(popens, timeoutms=1000):
+        h1.cmdPrint('tc -s class ls dev h1-eth0')
         if perf:
             print '<%s>: %s' % (perf, line)
         if time() >= endTime:
             print 'timeout'
             for p in popens.values():
                 p.send_signal( SIGINT )
+    '''
+
+    #TODO redirect iperf to files
+    #TODO parse files
+    #TODO display graph
 
     net.stop()
 
 if __name__ == '__main__':
     lg.setLogLevel('info')
-    rc3Test(LINK_BW_1, 20000000)
+    rc3Test(37, 20000)
+
+    plt.plot([1,2,3,4])
+    plt.ylabel('some numbers')
+    plt.show()
